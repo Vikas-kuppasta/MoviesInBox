a793fd8f7fa94545ff0cba0aad9401ce397a6131
Vikas-kuppasta <your-vikaskuppasta@gmail.com>
2025-06-27 16:03:52 +0530

    files added

---
+++ src/Components/Header/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from './logo.png'
+
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <div>
+                    <img  src={logo} alt="" />
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+
